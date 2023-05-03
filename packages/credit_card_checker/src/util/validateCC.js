8a3d0926a7f60e0d8f5173d4f04b60e2dac76dc7
bchooxg <56596358+bchooxg@users.noreply.github.com>
2023-05-03 18:08:24 +0800

    Added First Testcase

---
+++ packages/credit_card_checker/src/util/validateCC.js
@@ -0,0 +1,37 @@
+// Function to validate credit card number
+// The function should return:
+// - 'no-calc' if the input string is empty
+// - 'invalid' if the input string contains non-digit characters
+// - 'valid' if the Luhn checksum is correct
+// - 'invalid' if the Luhn checksum is incorrect
+// - 'invalid' if the Luhn checksum cannot be calculated
+export
+
+function validateCC(ccNumber) { // Returns True or False
+  // split digits into array
+  var ccNumberArray = ccNumber.split(''); // reverse array
+  let newNum = 0;
+  // console.log(`final arr ${ccNumberArray}`)
+  // double every second digit
+  for(let i = 0; i < ccNumberArray.length; i++) {
+    if(i % 2 == 0) {
+      newNum = ccNumberArray[i] * 2;
+      if(newNum > 9) {
+        newNum = newNum - 9;
+      }
+      ccNumberArray[i] = newNum;
+    }
+  }
+
+  // sum all digits
+  let sum = 0;
+  for(let i = 0; i < ccNumberArray.length; i++) {
+    sum += parseInt(ccNumberArray[i]);
+  }
+  // console.log(`final arr ${ccNumberArray}`)
+  // console.log(sum % 10 == 0)
+
+  // check if sum is divisible by 10
+  return sum % 10 == 0;
+
+}
